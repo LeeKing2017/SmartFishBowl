908b99f6bebf7ffde034d891d11bec7d8211082b
Minhyeok <danny9kwon@gmail.com>
2022-12-03 20:12:49 +0900

    APP_URL_수정

--- APP/SmartFishBowl/app/src/main/java/com/example/smartfishbowl/api/APIS.kt
+++ APP/SmartFishBowl/app/src/main/java/com/example/smartfishbowl/api/APIS.kt
@@ -6,10 +6,7 @@ import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface APIS {
     @POST("login") // 파이어베이스 디바이스 토큰값 전송
@@ -51,15 +48,13 @@ interface APIS {
         @Body jsonparams: FoodSetting
     ): Call<String>
 
-    @POST("signout")
-    @Headers("accept: application/json",
-        "content-type: application/json")
+    @GET("signout")
     fun signOut(
         @Header("Authorization") jwtToken: String
     ): Call<String>
 
     companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
-        private const val BASE_URL = "http://124.60.219.83:8080/" // 주소
+        private const val BASE_URL = "http://222.238.106.88:8080/" // 주소
 
         fun create(): APIS {
             val gson : Gson = GsonBuilder().setLenient().create()
